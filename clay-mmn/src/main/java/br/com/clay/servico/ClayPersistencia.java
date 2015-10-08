e1e5cb9ae02c961b2101ba657ca3f134eeab22d3
rafaelsvgomes <rafaelsvgomes@gmail.com>
2015-10-08 10:21:34 -0300

    Teste

--- clay-mmn/src/main/java/br/com/clay/servico/ClayPersistencia.java
+++ clay-mmn/src/main/java/br/com/clay/servico/ClayPersistencia.java
@@ -10,68 +10,66 @@ import javax.persistence.criteria.Root;
 import br.com.clay.entidade.ClayEntidade;
 
 /**
- * Classe resolve os métodos basicos de cadastro (CRUD) com API da
- * <code>JPA</code>.
+ * Classe resolve os métodos basicos de cadastro (CRUD) com API da <code>JPA</code>.
  * 
  */
 public abstract class ClayPersistencia<T extends ClayEntidade, PK extends Number> {
 
-	/**
-	 * Classe da entidade, necessário para o método
-	 * <code>EntityManager.find</code>.
-	 */
-	private Class<T> entityClass;
+    /**
+     * Classe da entidade, necessário para o método <code>EntityManager.find</code>.
+     */
+    private Class<T> entityClass;
 
-	public ClayPersistencia(Class<T> entityClass) {
-		this.entityClass = entityClass;
-	}
+    public ClayPersistencia(Class<T> entityClass) {
 
-	public T save(T e) {
-		if (e.getId() != null)
-			return getEntityManager().merge(e);
-		else {
-			getEntityManager().persist(e);
-			return e;
-		}
-	}
+        this.entityClass = entityClass;
+    }
 
-	public void remove(T entity) {
-		getEntityManager().remove(getEntityManager().merge(entity));
-	}
+    public T save(T e) {
+        if (e.getId() != null)
+            return getEntityManager().merge(e);
+        else {
+            getEntityManager().persist(e);
+            return e;
+        }
+    }
 
-	public T find(PK id) {
-		return getEntityManager().find(entityClass, id);
-	}
+    public void remove(T entity) {
+        getEntityManager().remove(getEntityManager().merge(entity));
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public List<T> findAll() {
-		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-		cq.select(cq.from(entityClass));
-		return getEntityManager().createQuery(cq).getResultList();
-	}
+    public T find(PK id) {
+        return getEntityManager().find(entityClass, id);
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public List<T> findRange(int[] range) {
-		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-		cq.select(cq.from(entityClass));
-		Query q = getEntityManager().createQuery(cq);
-		q.setMaxResults(range[1] - range[0]);
-		q.setFirstResult(range[0]);
-		return q.getResultList();
-	}
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public List<T> findAll() {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public int count() {
-		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-		Root<T> rt = cq.from(entityClass);
-		cq.select(getEntityManager().getCriteriaBuilder().count(rt));
-		Query q = getEntityManager().createQuery(cq);
-		return ((Long) q.getSingleResult()).intValue();
-	}
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public List<T> findRange(int[] range) {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        Query q = getEntityManager().createQuery(cq);
+        q.setMaxResults(range[1] - range[0]);
+        q.setFirstResult(range[0]);
+        return q.getResultList();
+    }
 
-	/**
-	 * Exige a definição do <code>EntityManager</code> responsável pelas
-	 * operaçõees de persistência.
-	 */
-	protected abstract EntityManager getEntityManager();
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public int count() {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        Root<T> rt = cq.from(entityClass);
+        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+        Query q = getEntityManager().createQuery(cq);
+        return ((Long) q.getSingleResult()).intValue();
+    }
+
+    /**
+     * Exige a definição do <code>EntityManager</code> responsável pelas operaçõees de persistência.
+     */
+    protected abstract EntityManager getEntityManager();
 }
